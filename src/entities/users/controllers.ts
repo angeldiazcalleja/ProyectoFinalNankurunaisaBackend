2d326b37fbc462cb5924f5161dd7561c226a1f23
Ángel <angeldiazcalleja@gmail.com>
2023-12-23 12:37:08 +0100

    feat:Create Register Controller

--- src/entities/users/controllers.ts
+++ src/entities/users/controllers.ts
@@ -31,3 +31,48 @@ export const login = async (req: Request, res: Response) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+export const register = async (req: Request, res: Response) => {
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{8,16}$/;
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  
+    const { name, surname, email, phone, role, password } = req.body;
+    const requestingUserRole = req.token?.role;
+  
+    if (!email || !password) {
+      return handleBadRequest(res);
+    }
+  
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        message: "Password does not meet the requirements. It must be between 8 and 16 characters, contain at least one uppercase letter, one digit, and one special character.",
+      });
+    }
+  
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Invalid email address format.",
+      });
+    }
+  
+    const userFound = await userExtendedModel.findOne({ email });
+  
+    if (userFound) {
+      return res.status(409).json({
+        message: "A user is already registered with that email address.",
+      });
+    }
+  
+    const hashedPassword = bcrypt.hashSync(password, CONF.HASH_ROUNDS);
+  
+    const newUser = new userExtendedModel({
+      name, surname, email, phone, password: hashedPassword, role: 'admin',
+    });
+  
+    const result = await newUser.save();
+  
+    return res.status(200).json({
+      message: "User registered successfully.",
+      userRegistered: result.toObject(),
+    });
+  };
